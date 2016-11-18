5bd57367c05fef8157d2417f83713d80076641d3
Cole Willsea <coleww@gmail.com>
2016-11-17 18:12:46 -0800

    remove un-needed data

--- src/data.js
+++ src/data.js
@@ -1,23 +1,14 @@
 const cardData = [
   {
     name: 'Murloc Warleader',
-    buff: 2,
-    attack: 3,
-    charge: false,
     imgSrc: 'warleader'
   },
   {
     name: 'Bluegill Warrior',
-    buff: 2,
-    attack: 2,
-    charge: false,
     imgSrc: 'warrior'
   },
   {
     name: 'Generic Murloc',
-    buff: 0,
-    attack: 0,
-    charge: false,
     imgSrc: 'generic'
   }
 ]
